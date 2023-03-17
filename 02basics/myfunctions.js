808a855b6615fc646f7b332d1f3cb39abcc1b286
Onur Bugdayci <bugdayci.onur@aol.com>
2023-03-17 11:59:33 +0100

    bettert way to write functions

--- 02basics/myfunctions.js
+++ 02basics/myfunctions.js
@@ -15,3 +15,22 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 loginUser("o", "o@o.b");
+// function getValue(myVal: number) {
+//     if (myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errMsg) {
+    console.log(errMsg);
+}
+function handleError(errMsg) {
+    throw new Error(errMsg);
+}
--- 02basics/myfunctions.ts
+++ 02basics/myfunctions.ts
@@ -1,4 +1,4 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
     return num + 2;
 }
 
@@ -20,5 +20,34 @@ signUpUser("onur", "onur@itenium.be", false);
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
 loginUser("o", "o@o.b")
+
+// function getValue(myVal: number) {
+//     if (myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+
+const getHello = (s: string): string => {
+    return "";
+}
+
+
+const heros = ["thor", "spiderman", "ironman"];
+
+heros.map(hero => {
+    return `hero is ${hero}`;
+});
+
+function consoleError(errMsg: string): void {
+    console.log(errMsg)
+}
+
+function handleError(errMsg: string): never { // good for handling errors, checks documentation
+    throw new Error(errMsg);
+}
+
+
+
 
 export {}
