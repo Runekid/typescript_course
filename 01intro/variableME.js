8aea0978fc33bb62a8fde0cd617d2c1e35763cf7
Onur Bugdayci <bugdayci.onur@aol.com>
2023-03-17 11:08:00 +0100

    primitive types and type inference

--- 01intro/variableME.js
+++ 01intro/variableME.js
@@ -3,3 +3,7 @@ exports.__esModule = true;
 var greetings = "Hello Onur";
 greetings.toLowerCase();
 console.log(greetings);
+// number + type inference (type is decided by the value that's being assigned)
+var userid = 3344.45;
+userid.toFixed();
+var isLoggedIn = false;
--- 01intro/variableME.ts
+++ 01intro/variableME.ts
@@ -4,4 +4,11 @@ greetings.toLowerCase();
 
 console.log(greetings);
 
+// number + type inference (type is decided by the value that's being assigned)
+let userid = 3344.45;
+
+userid.toFixed();
+
+let isLoggedIn: boolean = false;
+
 export {}
