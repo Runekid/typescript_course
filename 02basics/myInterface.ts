98e04fe0ee38f7c2fc68c8a0e24e3919716d318a
Runekid <bugdayci.onur@aol.com>
2023-03-20 11:39:52 +0100

    reopening an interface

--- 02basics/myInterface.ts
+++ 02basics/myInterface.ts
@@ -8,12 +8,17 @@ interface User {
     getCoupon(couponName: string, value: number): number 
 }
 
+interface User { // this is reopening an interface -> difference with type
+    ghToken: string
+}
+
 const onur: User = {dbId: 12, userId: 45, email: "onur@de.b", startTrial: () => {
     return "trial started"
 },
 getCoupon(couponName: "cou", val: 1) {
     return val
 },
+ghToken: "kl"
 }
 //onur.dbId = 56;
 onur.email = "test@g.n"
