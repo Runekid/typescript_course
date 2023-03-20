d3a7466e6025e408bfd512a5b2eed23de3280755
Runekid <bugdayci.onur@aol.com>
2023-03-20 11:21:29 +0100

    enums

---
+++ 02basics/myEnums.js
@@ -0,0 +1,7 @@
+// enum SeatChoice {
+//     AISLE = 10, //following enums are always incremental unless set otherwise
+//     MIDDLE = 22,
+//     WINDOW,
+//     FOURTH
+// }
+var hcSeat = "10" /* SeatChoice.AISLE */;
---
+++ 02basics/myEnums.ts
@@ -0,0 +1,15 @@
+// enum SeatChoice {
+//     AISLE = 10, //following enums are always incremental unless set otherwise
+//     MIDDLE = 22,
+//     WINDOW,
+//     FOURTH
+// }
+
+const enum SeatChoice { // const will just assign value to variable
+    AISLE = "10", // when you set a value other than number than all values need to be set manually if the order is not clear for incrementation
+    MIDDLE = 22,
+    WINDOW,
+    FOURTH
+}
+
+const hcSeat = SeatChoice.AISLE;
