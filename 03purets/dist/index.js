8a792f09861354fe2977586ef39d9991b9ff8111
Runekid <bugdayci.onur@aol.com>
2023-03-20 12:40:34 +0100

    getters and setters

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -12,7 +12,23 @@ class User {
     constructor(name, email) {
         this.name = name;
         this.email = email;
+        this._courseCount = 1;
         this.city = "";
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= ) {
+            throw new Error("course count should be more than ");
+        }
+        this._courseCount = courseNum;
+    }
+    deleteToken() {
+        console.log("token deleted");
     }
 }
 const onur = new User("Onur", "onur@dev.be");
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -9,11 +9,33 @@
 // }
 
 class User {
+
+    private _courseCount: number = 1
+
     city: string = "";
     constructor(public name: string,
          public email: string,
          //private userId: string
          ) {
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum: number) { // setters have no return type!
+        if (courseNum <= ) {
+            throw new Error("course count should be more than ")
+        }
+        this._courseCount = courseNum;
+    }
+
+    private deleteToken() {
+        console.log("token deleted")
     }
 }
 
