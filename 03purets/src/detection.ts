006855eb592e608cc3e12252a3ec6b6c9311f0ee
Runekid <bugdayci.onur@aol.com>
2023-03-21 10:41:06 +0100

    discriminated union and exhaustiveness checking

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -70,3 +70,44 @@ function getFood(pet:Fish | Bird) {
         return "bird food"
     }
 }
+
+interface Circle {
+    kind: "circle"
+    radius: number
+}
+
+interface Square {
+    kind: "square"
+    side: number
+}
+
+interface Rectangle {
+    kind: "rectangle"
+    length: number
+    width: number
+}
+
+type Shape = Circle | Square | Rectangle; // Discriminated union
+
+function getTrueShape(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2
+    }
+
+    //return shape.side * shape.side
+}
+
+function getArea( shape: Shape) { // exhaustiveness checking
+    switch(shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2
+        case "square":
+            return shape.side * shape.side
+        case "rectangle":
+            return shape.length * shape.width
+        default:
+            const _defaultForShape: never = shape
+            return _defaultForShape
+    }
+}
+
